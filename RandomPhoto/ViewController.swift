9203e1d2b65c20fd14d9c974cc57f2da20c8a51f
Aathma Muruganathan <75296425+DjMurugu@users.noreply.github.com>
2023-10-16 11:33:50 -0700

    Update ViewController.swift

--- RandomPhoto/ViewController.swift
+++ RandomPhoto/ViewController.swift
@@ -1,21 +1,13 @@
-//
-//  ViewController.swift
-//  RandomPhoto
-//
-//  Created by Tsuen Hsueh on 2021/9/11.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
     private let imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
         imageView.backgroundColor = .white
         return imageView
     }()
-    
+
     private let button: UIButton = {
         let button = UIButton()
         button.backgroundColor = .white
@@ -23,30 +15,19 @@ class ViewController: UIViewController {
         button.setTitleColor(.black, for: .normal)
         return button
     }()
-    
-    let colors: [UIColor] = [
-        .systemGray,
-        .systemRed,
-        .systemOrange,
-        .systemTeal,
-        .systemGreen,
-        .systemBlue,
-        .systemPink,
-        .systemYellow,
-        .systemPurple,
-        .systemIndigo
-    ]
-    
+
+    // Replace this with your array of image names
+    let imageNames: [String] = ["image1", "image2", "image3"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // change the background color
         view.backgroundColor = .systemPink
         view.addSubview(imageView)
         imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
         imageView.center = view.center
-        
+
         view.addSubview(button)
-        
+
         getRandomPhoto()
         button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
     }
@@ -55,30 +36,23 @@ class ViewController: UIViewController {
         getRandomPhoto()
         view.backgroundColor = colors.randomElement()
     }
-    
+
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         button.frame = CGRect(
             x: 30,
-            y: view.frame.size.height-150-view.safeAreaInsets.bottom,
-            width: view.frame.size.width-60,
+            y: view.frame.size.height - 150 - view.safeAreaInsets.bottom,
+            width: view.frame.size.width - 60,
             height: 55
         )
     }
-    
+
     func getRandomPhoto() {
-      //  var random = arc4random_uniform(0)
-        //switch random {
-       // case 0:
-       let imageView = UIImage(named: "600px-Download-Icon.png")
-        
-      //  case 1:
-      //  case 2:
-            
-       // }
-
-        
+        // Get a random image name from your array
+        if let randomImageName = imageNames.randomElement() {
+            if let image = UIImage(named: randomImageName) {
+                imageView.image = image
+            }
+        }
     }
-
-
 }
